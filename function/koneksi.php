b44ac1ab980faebb6ea4a15c8cfecd4c4ada03ec
sharenalicia0602 <sharenalicia0602@gmail.com>
2020-04-27 22:16:48 +0700

    Project E-Commerce

---
+++ function/koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "wego_weshop";
+
+	$koneksi = mysqli_connect($server, $username, $password, $database) or die ("koneksi ke database gagal")
+
+ ?>
+
+
+ 
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+
+	session_start();
+
+	include_once("function/helper.php");
+	include_once("function/koneksi.php");
+
+	$page = isset($_GET['page']) ? $_GET['page'] : false;
+	
+	$user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
+	$nama = isset($_SESSION['nama']) ? $_SESSION['nama'] : false;
+	$level = isset($_SESSION['level']) ? $_SESSION['level'] : false;
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Weshop | Barang-Barang Elektronik</title>
+ 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL. "css/style.css"; ?>">
+ </head>
+ <body>
+
+ 	<div id="container">
+ 		<div id="header">
+ 			<a href="<?php echo BASE_URL. "index.php"; ?>">
+ 				<img src="<?php echo BASE_URL. "image/logo.png"; ?>">
+ 			</a>
+ 			<div id="menu">
+ 				<div id="user">
+ 					<?php
+							if($user_id){
+								echo "Hi <b>$nama</b>,
+									  <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile</a>
+									  <a href='".BASE_URL."logout.php'>Logout</a>";
+							}else{
+								echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
+									 <a href='".BASE_URL."index.php?page=register'>Register</a>";
+							}
+						?>
+ 				</div>
+ 				<a href="<?php echo BASE_URL. "index.php?page=keranjang"; ?>" id="button-keranjang">
+	 				<img src="<?php echo BASE_URL. "image/cart.png"; ?>">
+	 			</a>
+ 			</div>
+ 		</div>
+
+ 		<div id="content">
+ 			<?php 
+ 				$filename = "$page.php";
+
+ 				if(file_exists($filename)){
+ 					include_once ("$filename");
+ 				}
+ 				else{
+ 					echo "Maaf, file tersebut tidak ada didalam sistem";
+ 				}
+ 			 ?>
+ 		</div>
+ 		<div id="footer">
+ 			<p>copyright Weshop | 2016</p>
+ 		</div>
+ 	</div>
+
+ </body>
+ </html>
